ad93ca176840c83a47ad8c38edae3300ead14506
cristian2294 <doko.stan@gmail.com>
2021-09-16 22:40:44 -0500

    splash screen added

--- app/src/main/java/com/example/pokedexapp/MainActivity.kt
+++ app/src/main/java/com/example/pokedexapp/MainActivity.kt
@@ -1,48 +1,26 @@
 package com.example.pokedexapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import com.example.pokedexapp.databinding.ActivityMainBinding
+import com.example.pokedexapp.model.APIService
 import retrofit2.*
 import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
     private lateinit var binding:ActivityMainBinding
-    private lateinit var resultList: ArrayList<Result>
     override fun onCreate(savedInstanceState: Bundle?) {
+        Thread.sleep(2000)
+        setTheme(R.style.SplashTheme)
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-
-        val retrofit = getRetrofit()
-        val service = retrofit.create(APIService::class.java)
-        val call =  service.getAllPokemons(150,0)
-
-        call.enqueue(object : Callback<PokeResponse>{
-            override fun onResponse(call: Call<PokeResponse>, response: Response<PokeResponse>) {
-
-                if (response.isSuccessful){
-                    response.body()?.results?.let { list ->
-
-                        // FILL THE RECYCLERVIEW
-                        Log.d("RESULT", response.body().toString())
-                    }
-                }
-            }
-            override fun onFailure(call: Call<PokeResponse>, t: Throwable) {
-                Log.d("Error", ""+t.printStackTrace())
-            }
-
-        })
+        val intent = Intent(this, PokemonActivity::class.java)
+        startActivity(intent)
     }
 
-    private fun getRetrofit():Retrofit{
-        return Retrofit.Builder()
-            .baseUrl("https://pokeapi.co/api/v2/")
-            .addConverterFactory(GsonConverterFactory.create())
-            .build()
-    }
 }
--- app/src/main/java/com/example/pokedexapp/model/APIService.kt
+++ app/src/main/java/com/example/pokedexapp/model/APIService.kt
@@ -1,19 +1,12 @@
-package com.example.pokedexapp
+package com.example.pokedexapp.model
 
+import com.example.pokedexapp.PokeResponse
 import retrofit2.Call
 import retrofit2.http.GET
 import retrofit2.http.Query
-import retrofit2.http.Url
 
 interface APIService {
     @GET("pokemon?")
     fun getAllPokemons(@Query("limit") limit:Int,
                        @Query("offset")offset: Int): Call<PokeResponse>
-
-
-    /*
-    @GET("pokemon?limit=5&offset=0")
-    fun getAllPokemons(): Call<List<Result>>
-
-     */
 }
